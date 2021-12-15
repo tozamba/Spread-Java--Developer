57e8a4fb10f096ef794ac3f8e4a00a56ad44c565
tozamba <tozamba@hotmail.com>
2021-12-14 21:12:07 -0300

    Estrutura de repetição While para nome e idade.
    Estrutura de repetição While para nome e idade. Sem armazenamento.
    Para parar digitar 0.

---
+++ Ex1_Nome_E_Idade.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+import java.util.Scanner;
+
+public class Ex1_Nome_E_Idade {
+    public static void  main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while(true){
+            System.out.println("Nome: ");
+            nome = scan.next();
+            if(nome.equals("0")) break;
+            System.out.println("Idade: ");
+            idade = scan.nextInt();
+        }
+    }
+}
